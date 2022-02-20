520b0eb5b49886116a476fc7d60c12976f09d1fd
Michael Bianco <iloveitaly@gmail.com>
2022-02-20 06:47:34 -0700

    Initial implementation

---
+++ cli.py
@@ -0,0 +1,30 @@
+import os
+
+import click
+from dotenv import load_dotenv
+import pyjson5 as json
+import main
+
+load_dotenv()
+
+
+@click.command(help="Organizes todoist tasks based on custom rules")
+@click.option("--task-limit", default=20, help="Total task limit for the day", show_default=True, type=int)
+@click.option("--default-filter", default="(today | overdue) & !assigned to:others & !recurring", help="Default todoist filter", show_default=True, type=str)
+@click.option("--filter-json", default="filters.json", help="Default todoist filter", show_default=True, type=str)
+@click.option("--dry-run", default=False, help="Dry run the task updates", show_default=True, type=bool)
+@click.option("--api-key", default=None, help="API key. Sourced from TODOIST_API_KEY as well")
+def cli(**kwargs):
+    if not kwargs["api_key"]:
+        kwargs["api_key"] = os.getenv("TODOIST_API_KEY")
+
+    with open(kwargs['filter_json']) as f:
+        kwargs["rules"] = json.load(f)
+
+    del kwargs['filter_json']
+
+    main.apply_todoist_filters(**kwargs)
+
+
+if __name__ == "__main__":
+    cli()
---
+++ main.py
@@ -0,0 +1,80 @@
+from todoist_api_python.api import TodoistAPI
+
+# TODO document this as a way to cleanup ipython backtraces
+# https://cs.github.com/ipython/ipython/blob/46a51ed69cdf41b4333943d9ceeb945c4ede5668/IPython/core/crashhandler.py#L225
+import IPython.core.crashhandler
+IPython.core.crashhandler.crash_handler_lite = lambda one, two, three: None
+
+# from rich.pretty import pprint
+# how can I make this the default printer?
+
+# example rules json:
+# rules = [
+#     {
+#         'filter': '@research',
+#         'limit': 3
+#     },
+#     {
+#         'filter': '#Networking',
+#         'limit': 2
+#     }
+# ]
+
+import random
+import logging, os, sys
+logger = logging.getLogger(__name__)
+logging.basicConfig(datefmt='%m-%d %H:%M', format='%(asctime)s %(levelname)-8s %(message)s')
+
+# run with LOG_LEVEL=DEBUG
+# https://stackoverflow.com/questions/11548674/logging-info-doesnt-show-up-on-console-but-warn-and-error-do
+log_level = os.environ.get("LOG_LEVEL", "INFO")
+logger.setLevel(getattr(logging, log_level.upper(), logging.INFO))
+
+def apply_todoist_filters(task_limit, api_key, rules, dry_run, default_filter):
+    api = TodoistAPI(api_key)
+
+    if not default_filter:
+        default_filter = "(today | overdue) & !assigned to:others & !recurring"
+
+    labels = api.get_labels()
+
+    # tasks = api.get_tasks(filter=default_filter)
+
+    import datetime
+    # if sunday (day off) force all non-essenltial tasks to zero
+    is_sunday = datetime.date.today().weekday() == 6
+
+    for rule in rules:
+        filter_with_label = f'{default_filter} & {rule["filter"]}'
+        logger.debug(filter_with_label)
+
+        tasks_with_label = api.get_tasks(filter=filter_with_label)
+        low_priority_tasks = [task for task in tasks_with_label if task.priority == 1]
+
+        # randomize the task order so different tasks are displayed for completion each day
+        random.shuffle(low_priority_tasks)
+
+        # if sunday, force limit to 0
+        limit_for_filter = 0 if is_sunday else rule["limit"]
+
+        # after excluding the high-pri tasks, how many slots do we have left for this rule?
+        remaining_tasks = limit_for_filter - (len(tasks_with_label) - len(low_priority_tasks))
+        logger.debug(f"{remaining_tasks} tasks remaining for {rule['filter']}")
+
+        for low_priority_task in low_priority_tasks:
+            if remaining_tasks <= 0:
+                logger.info("punting task %s", low_priority_task.content)
+
+                if not dry_run:
+                    api.update_task(low_priority_task.id, due_string="tomorrow")
+            else:
+                logger.debug("leaving task %s", low_priority_task.content)
+                remaining_tasks -= 1
+
+
+    all_remaining_tasks = api.get_tasks(filter=default_filter)
+
+    # TODO what's the point of this?
+    if len(all_remaining_tasks) > task_limit:
+        logger.warn("many remaining tasks left, improve filtering")
+
