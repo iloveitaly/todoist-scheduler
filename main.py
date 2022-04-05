68c1de7e5b62d05ff48ae8c2e5c0f5a99147bf30
Michael Bianco <iloveitaly@gmail.com>
2022-04-05 08:45:43 -0600

    Black reformatting

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from todoist_api_python.api import TodoistAPI
 # TODO document this as a way to cleanup ipython backtraces
 # https://cs.github.com/ipython/ipython/blob/46a51ed69cdf41b4333943d9ceeb945c4ede5668/IPython/core/crashhandler.py#L225
 import IPython.core.crashhandler
+
 IPython.core.crashhandler.crash_handler_lite = lambda one, two, three: None
 
 # from rich.pretty import pprint
@@ -11,15 +12,21 @@ IPython.core.crashhandler.crash_handler_lite = lambda one, two, three: None
 import random
 import datetime
 import logging, os, sys
+
 logger = logging.getLogger(__name__)
-logging.basicConfig(datefmt='%m-%d %H:%M', format='%(asctime)s %(levelname)-8s %(message)s')
+logging.basicConfig(
+    datefmt="%m-%d %H:%M", format="%(asctime)s %(levelname)-8s %(message)s"
+)
 
 # run with LOG_LEVEL=DEBUG
 # https://stackoverflow.com/questions/11548674/logging-info-doesnt-show-up-on-console-but-warn-and-error-do
 log_level = os.environ.get("LOG_LEVEL", "INFO")
 logger.setLevel(getattr(logging, log_level.upper(), logging.INFO))
 
-def apply_todoist_filters(task_limit, api_key, rules, dry_run, punt_time, default_filter):
+
+def apply_todoist_filters(
+    task_limit, api_key, rules, dry_run, punt_time, default_filter
+):
     api = TodoistAPI(api_key)
 
     if not default_filter:
@@ -55,7 +62,9 @@ def apply_todoist_filters(task_limit, api_key, rules, dry_run, punt_time, defaul
         limit_for_filter = 0 if is_sunday else rule["limit"]
 
         # after excluding the high-pri tasks, how many slots do we have left for this rule?
-        remaining_tasks = limit_for_filter - (len(tasks_with_label) - len(low_priority_tasks))
+        remaining_tasks = limit_for_filter - (
+            len(tasks_with_label) - len(low_priority_tasks)
+        )
         logger.debug(f"{remaining_tasks} tasks remaining for {rule['filter']}")
 
         for low_priority_task in low_priority_tasks:
@@ -68,10 +77,8 @@ def apply_todoist_filters(task_limit, api_key, rules, dry_run, punt_time, defaul
                 logger.debug("leaving task %s", low_priority_task.content)
                 remaining_tasks -= 1
 
-
     all_remaining_tasks = api.get_tasks(filter=default_filter)
 
     # TODO what's the point of this?
     if len(all_remaining_tasks) > task_limit:
         logger.warn("many remaining tasks left, improve filtering")
-
