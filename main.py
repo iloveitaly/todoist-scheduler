ce12c83e21de93636b44201c235d2d0c33ae216e
Michael Bianco <iloveitaly@gmail.com>
2024-02-09 06:48:50 -0700

    fix: handle successful run

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import os
 
 from apscheduler.schedulers.background import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
+import click
 
 from todoist_scheduler import cli
 
 
 def job():
-    cli()
+    """
+    this is me being lazy, I don't want to have to define another entrypoint, so I'm just catching the successful outcome
+    """
+    try:
+        cli()
+    except click.exceptions.Exit as e:
+        if e.exit_code != 0:
+            raise
 
 
 def cron():
